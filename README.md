8dba9bc47646a2754a66e01f5927219e7a633b1d
Mamontov <mammmoth@list.ru>
2024-11-18 02:18:22 +0300

    teloxide prep

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ![alt text](./McDev_thin_900x70.png "McDev_thin_900x70.png")
+
+
+
 # passgen-telegram
 Telegram bot backend system for easy convenient and strong passwords generation with admin web interface.
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::path;
+use std::path::PathBuf;
 use log::*;
 use teloxide::dispatching::{Dispatcher, HandlerExt, UpdateFilterExt};
 use teloxide::dptree::{case, endpoint};
@@ -5,6 +7,8 @@ use teloxide::error_handlers::LoggingErrorHandler;
 use teloxide::{prelude::*, utils::command::BotCommands, update_listeners::webhooks};
 use teloxide::requests::Requester;
 use teloxide::{dptree, respond, Bot};
+use teloxide::payloads::SendPhoto;
+use teloxide::types::{InlineKeyboardButton, InlineKeyboardMarkup, InputFile, ReplyMarkup};
 
 #[tokio::main]
 async fn main() {
@@ -14,16 +18,29 @@ async fn main() {
     let bot = Bot::new("7745281341:AAFWcFzoCL0KK2HjyblVN7n1zQL4AUvHsD0");
     //dbg!(bot.get_me().await).expect("TODO: panic message");
 
-    let addr = ([192, 168, 0, 10], 8000).into();
-    let url = "https://passgen.mamont.xyz/telegram/webhooking".parse().unwrap();
+
+    let addr = ([0, 0, 0, 0], 8000).into();
+    let url = "https://tg.passgen.mamont.xyz/telegram/webhooking".parse().unwrap();
     let listener = webhooks::axum(bot.clone(), webhooks::Options::new(addr, url))
         .await
         .expect("Couldn't setup webhook");
 
+
     teloxide::repl_with_listener(
         bot,
         |bot: Bot, msg: Message| async move {
-            bot.send_message(msg.chat.id, "hop").await?;
+            //let logo = "<a href=\"https://passgen.mamont.xyz/McDev_640x360.png\">logo</a>";
+            let inline_btns = ["⚙Generate password"]
+                .map(|btn| InlineKeyboardButton::callback(btn, btn));
+            println!("{}", msg.text().clone().unwrap().to_string());
+
+            bot.send_message(msg.chat.id, "<b>Menu:</b>")
+                .parse_mode("HTML".parse().unwrap())
+                .reply_markup(InlineKeyboardMarkup::new([inline_btns]))
+                .await?;
+            bot.send_message(msg.chat.id, "pwd-bla-bla").await?;
+
+
             Ok(())
         },
         listener,
